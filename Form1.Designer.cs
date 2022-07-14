0da91d93f98167614ac067ad894fdbac94505998
MenatniPaulo <34354716+MentaniPaulo@users.noreply.github.com>
2022-07-13 23:55:59 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace FuntionGraph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.tb_funtion = new System.Windows.Forms.TextBox();
+            this.btn_add_funtion = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cartesianChart1.Location = new System.Drawing.Point(0, 0);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(800, 450);
+            this.cartesianChart1.TabIndex = 0;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.tb_funtion);
+            this.flowLayoutPanel1.Controls.Add(this.btn_add_funtion);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(181, 450);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // tb_funtion
+            // 
+            this.tb_funtion.Location = new System.Drawing.Point(3, 16);
+            this.tb_funtion.Name = "tb_funtion";
+            this.tb_funtion.Size = new System.Drawing.Size(178, 20);
+            this.tb_funtion.TabIndex = 0;
+            // 
+            // btn_add_funtion
+            // 
+            this.btn_add_funtion.Location = new System.Drawing.Point(3, 42);
+            this.btn_add_funtion.Name = "btn_add_funtion";
+            this.btn_add_funtion.Size = new System.Drawing.Size(178, 23);
+            this.btn_add_funtion.TabIndex = 1;
+            this.btn_add_funtion.Text = "Adicionar";
+            this.btn_add_funtion.UseVisualStyleBackColor = true;
+            this.btn_add_funtion.Click += new System.EventHandler(this.btn_add_funtion_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Adicione uma função";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.cartesianChart1);
+            this.Name = "Form1";
+            this.Text = "Função Grafico";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_funtion;
+        private System.Windows.Forms.Button btn_add_funtion;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using LiveCharts;
+using LiveCharts.Configurations;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace FuntionGraph
+{
+    public partial class Form1 : Form
+    {
+        //List<int> x = new List<int>();
+        string funtioo;
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            var values = new ChartValues<Point>();
+            var values2 = new ChartValues<Point>();
+            // Create sine graph
+            
+            
+            graphDraw(funtion(),"s");
+
+        }
+
+        private void graphDraw(ChartValues<Point> values, string name)
+        {
+            cartesianChart1.Series.Add(new LineSeries
+            {
+                Configuration = new CartesianMapper<Point>()
+                .X(point => point.X) // Define a function that returns a value that should map to the x-axis
+                .Y(point => point.Y), // Define a function that returns a value that should map to the y-axis
+                Title = name,
+                Values = values,
+                //Stroke = System.Windows.Media.Brushes.Red,
+                PointGeometry = null
+            });
+
+        }
+
+        private ChartValues<Point> funtion()
+        {
+            var values = new ChartValues<Point>();
+            for (double n = -10; n < 10; n++)
+            {
+                var point = new Point() { X = (int)n, Y = (int)(n * n) };
+                values.Add(point);
+            }
+            return values;
+        }
+
+        private void btn_add_funtion_Click(object sender, EventArgs e)
+        {
+            funtioo = tb_funtion.Text;
+            tb_funtion.Focus();
+        }
+    }
+
+   
+}
